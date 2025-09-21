6257ce16cc5fd75324525cef222c2cd6f1ff7dc9
Mosab-kazeha <mk.lk.mosab@gmail.com>
2025-09-21 19:19:42 +0300

    add class to the folder and start work

---
+++ lib/core/resources/router/router_config.main.dart
@@ -0,0 +1,7 @@
+part of 'router_config.dart';
+
+abstract final class AppRouter {
+  static final router = GoRouter(
+    routes: [],
+  );
+}
---
+++ lib/core/resources/theme/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+static  ThemeData lightMode = ThemeData.light().copyWith();
+ static ThemeData darkMode = ThemeData.dark().copyWith();
+}
